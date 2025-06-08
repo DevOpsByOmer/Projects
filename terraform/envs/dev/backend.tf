8e253682d27903589442a61025175530d2d1d15c
Umar <dvop.omer@gmail.com>
2025-06-08 23:16:28 +0530

    Infra for order-service

--- terraform/envs/dev/backend.tf
+++ terraform/envs/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket2xxsa"
     key            = "envs/dev/terraform.tfstate" # path inside the bucket
-    region         = "us-east-1"
+    region         = "ap-south-1"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
