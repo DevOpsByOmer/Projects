705f527690cc569047a8f13b22b7ef4c11d6bd1b
Umar <dvop.omer@gmail.com>
2025-06-14 01:10:27 +0530

    added cicd for application 2

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -12,23 +12,22 @@ resource "aws_internet_gateway" "this" {
     Name = "${var.vpc_name}-igw"
   }
 }
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.this.id
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
 
   tags = {
-    Name = "${var.vpc_name}-public-rt"
+    Name = "${var.vpc_name}-nat"
   }
-}
-resource "aws_route" "internet_access" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.this.id
-}
 
-resource "aws_route_table_association" "public" {
-  count          = length(aws_subnet.public)
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
+  depends_on = [aws_internet_gateway.this]
+}
+resource "aws_eip" "nat" {
+  vpc = true
+
+  tags = {
+    Name = "${var.vpc_name}-nat-eip"
+  }
 }
 
 resource "aws_subnet" "public" {
@@ -53,3 +52,48 @@ resource "aws_subnet" "private" {
     Name = "private-subnet-${count.index + 1}"
   }
 }
+
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.vpc_name}-public-rt"
+  }
+}
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.vpc_name}-private-rt"
+  }
+}
+
+
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+resource "aws_route" "private_nat_route" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.this.id
+}
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+
+
+
+
+
